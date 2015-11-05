d6f3ccc81b6586c4cda2aefdadc3ed910458d329
flaturtha <talesofmurderpress@gmail.com>
2015-11-04 19:58:06 -0600

    dashboard v1

--- both/router/routes.js
+++ both/router/routes.js
@@ -1,6 +1,15 @@
 Router.route('/', {
   name: 'home'
 });
+Router.route('/books', {
+  name: 'books'
+});
+Router.route('/tasks', {
+  name: 'tasks'
+});
+Router.route('/archives', {
+  name: 'archives'
+});
 
 Router.route('/dashboard', {
   name: 'dashboard',
